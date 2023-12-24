cfedf4be6adb42e14729c922cc6f4450a5f7bdd4
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2023-12-24 18:26:22 +0100

    Lightbox task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function renderGallery(galleryItems) {
+  galleryList.innerHTML = galleryItems.map(getImageHtml).join("");
+}
+
+function getImageHtml({ preview, original, description }) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+          </li>`;
+}
