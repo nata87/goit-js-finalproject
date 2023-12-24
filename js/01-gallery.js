81f9a629675fef86018b80b2ed8c0df4498d16f3
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2023-12-24 18:02:15 +0100

    gallery Image

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+let lightBoxInstance;
+
+function renderGallery(galleryItems) {
+  galleryList.innerHTML = galleryItems.map(getImageHtml).join("");
+}
+
+function getImageHtml({ preview, original, description }) {
+  return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-original="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>`;
+}
+
+function closeLightBoxHandler(event) {
+  if (event.code === "Escape" && lightBoxInstance) {
+    lightBoxInstance.close();
+  }
+}
+
+renderGallery(galleryItems);
+
+galleryList.addEventListener("click", handleImageClick);
+
+function handleImageClick(e) {
+  e.preventDefault();
+
+  const image = e.target;
+
+  if (!image.classList.contains("gallery__image")) {
+    return;
+  }
+
+  lightBoxInstance = basicLightbox.create(
+    `
+        <img src="${image.dataset.original}">
+    `,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeLightBoxHandler);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", closeLightBoxHandler);
+      },
+    }
+  );
+
+  lightBoxInstance.show();
+}
